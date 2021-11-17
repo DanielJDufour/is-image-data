f4271d7539c5db0109ebaa82e0fd5fd2f9329732
DanielJDufour <daniel.j.dufour@gmail.com>
2021-11-17 12:23:05 -0500

    trimmed test

--- test.js
+++ test.js
@@ -2,18 +2,6 @@ const test = require("flug");
 
 const { isImageData, isRGB, isRGBA } = require("./is-image-data.js");
 
-const saveAndDisplay = async (filename, buf) => {
-  fs.writeFileSync(filename, buf);
-  const terminalImage = await import("terminal-image");
-  console.log(await terminalImage.default.buffer(buf));
-};
-
-const findAndReadImage = async (filename) => {
-  const buf = await findAndRead(filename);
-  const { height, width, pixels } = await readim({ data: buf });
-  return { height, width, data: pixels };
-};
-
 test("isColorStats JPG", async ({ eq }) => {
   const stats = [
     { min: 3, max: 218 },
